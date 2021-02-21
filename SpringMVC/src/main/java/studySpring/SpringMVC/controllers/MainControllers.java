4bd7628fa72f8a28805eb0ac7d9c99e57d760ba3
olegprokhorov <igarik10@yandex.ru>
2021-02-21 11:53:50 +0700

    spring study

---
+++ SpringMVC/src/main/java/studySpring/SpringMVC/controllers/MainControllers.java
@@ -0,0 +1,18 @@
+package studySpring.SpringMVC.controllers;
+
+
+import org.springframework.stereotype.Controller;
+        import org.springframework.ui.Model;
+        import org.springframework.web.bind.annotation.GetMapping;
+        import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainControllers {
+
+    @GetMapping("/")
+    public String greeting(Model model) {
+        model.addAttribute("title", "Main page");
+        return "home";
+    }
+
+}
