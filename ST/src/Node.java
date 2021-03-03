a6d08e26796852ae86769a48ffa8dd6496722fc6
olegprokhorov <igarik10@yandex.ru>
2021-03-04 00:50:22 +0700

    STree vis

---
+++ ST/src/Node.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public   class Node {
+    String sub = "";                       // a substring of the input string
+    List<Integer> ch = new ArrayList<>();  // list of child nodes
+}
